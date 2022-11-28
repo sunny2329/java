2c2a9fe6fabd4060122208faf35348fe43a6a6a3
Laura Moise <laura.moise@gmail.com>
2022-11-28 16:59:28 -0600

    parallel computing

--- demo/SwitchXP.java
+++ demo/SwitchXP.java
@@ -1,10 +1,20 @@
+/* Read day of week in Java 
+   https://docs.oracle.com/javase/8/docs/api/java/time/DayOfWeek.html
+*/
+import java.time.DayOfWeek
+import java.util.Calendar;
 
 public class SwitchXP {
-  public enum Day { SUNDAY, MONDAY, TUESDAY,WEDNESDAY, THURSDAY, FRIDAY, SATURDAY }
   public static void main(String args[]) {
-    var day = Day.SATURDAY;
+    Calendar calendar = Calendar.getInstance();
+    int today = calendar.get(Calendar.DAY_OF_WEEK);
+    var day = DayOfWeek.of(today);
     var isWeekend = switch (day) {
-            case  MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY -> false;
+            case  MONDAY, 
+                  TUESDAY, 
+                  WEDNESDAY, 
+                  THURSDAY, 
+                  FRIDAY       -> false;
             case  SUNDAY       -> true;
             case  SATURDAY     -> true;
             default -> throw new IllegalStateException("Invalid day: " + day);
---
+++ demo/multithread.java
@@ -0,0 +1,71 @@
+<html><head>
+  <meta charset="utf-8">
+  <meta name="description" content="Java programming free tutorial by Sage-Code.">
+  <meta name="author" content="Elucian Moise">
+  <meta name="keywords" content="language, syntax, programming, tutorial">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Website title -->
+  <title>Java Tutorial</title>  
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+  <!-- Icon -->
+  <link rel="icon" type="image/png"  href="../images/favicon.ico">  
+  <!-- Prism Highlighter -->
+  <link rel="stylesheet" href="../prism.css">
+  <script src="../prism.js"></script>
+  <!-- custom css -->
+  <link rel="stylesheet" href="../sage.css">
+  </head>
+<body>
+
+<div class="container">
+        
+<!-- header -->
+<div class="row">
+    <div class="col">
+        <a href="https://sagecode.net"><img src="../images/sage-logo.svg" alt="Sage-Code Laboratory" height="80"/></a>
+    </div>  
+    <div class="col  bottom-right">
+        <a href="index.html#lang-index">index</a>&lt;--
+    </div>
+</div>    
+
+<hr>
+
+<h2>Java Tutorial</h2>
+
+<div class="alert alert-secondary shadow-sm">
+Work in progress!
+</div>
+
+
+<div align="center">
+  <img src="../images/ladder.svg" alt="decision ladder"
+       width="640" class="img-fluid protect rounded shadow border" >
+  <p>Decision Ladder</p>
+</div>
+
+<pre class="line-numbers"><code class="language-java"
+>/* java code */
+</code></pre>
+
+<pre><code class="language-output">
+&gt; hello world
+</code></pre>
+
+
+<!-- work in progress-->
+
+<hr>
+<p><b>Read next:</b>
+<a href="template.html">Page Name</a></p>
+
+<!-- Footer -->
+<footer class="footer">
+  <div class="footer-copyright text-center"></div>
+</footer>
+</div>
+</body>
+</html>
